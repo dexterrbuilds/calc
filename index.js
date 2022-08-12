089e237eac901e911fe03ec304da6c3e81facece
thetechrat <ogunyemialameen@gmail.com>
2022-08-12 04:05:27 -0700

    updates

---
+++ index.js
@@ -0,0 +1,12 @@
+let displayEl = document.querySelector(".display");
+displayEl.textContent = [];
+
+function ac() {
+  displayEl.textContent = [];
+}
+
+function seven() {
+  let seven = document.getElementById("seven");
+  sevenEl = seven.textContent;
+  displayEl.textContent = [sevenEl];
+}
