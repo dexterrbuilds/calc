8066a15003307f398ae643c381bfb8e579f1ba36
thetechrat <ogunyemialameen@gmail.com>
2022-08-12 23:31:52 -0700

    updates

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// NOTE:
-// This is the final source code file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
-
 const equal = (n1, operator, n2) => {
   let result = "";
   if (operator === "add") {
